e2075fba1070f429f6947b5261e64bcaba084124
Gerardo Fernández <gefe.86@gmail.com>
2020-05-03 18:53:50 +0200

    feature: inject services

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
-
     /**
      * @Route("/library/list", name="library_list")
      */
-    public function list()
+    public function list(Request $request, LoggerInterface $logger)
     {
+        $title = $request->get('title', 'Alegría');
+        $logger->info('List action called 2');
         $response = new JsonResponse();
         $response->setData([
             'success' => true,
@@ -25,6 +28,10 @@ class LibraryController extends AbstractController
                 [
                     'id' => 2,
                     'title' => 'El nombre del viento'
+                ],
+                [
+                    'id' => 3,
+                    'title' => $title
                 ]
             ]
         ]);
