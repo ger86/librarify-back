bc0fdca6b9dd8011526405b9b39eb75c57ddfa1f
Gerardo Fernández <gefe.86@gmail.com>
2020-05-24 18:02:04 +0200

    feature: added book entity

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,27 +13,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class LibraryController extends AbstractController
 {
     /**
-     * @Route("/library/list", name="library_list")
+     * @Route("/books", name="books_get")
      */
-    public function list(Request $request, LoggerInterface $logger)
+    public function list(Request $request, BookRepository $bookRepository)
     {
-        $title = $request->get('title', 'Alegría');
-        $logger->info('List action called 2');
+        $books = $bookRepository->findAll();
+        $booksAsArray = [];
+        foreach ($books as $book) {
+            $booksAsArray[] = [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'image' => $book->getImage()
+            ];
+        };
         $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => $booksAsArray
+        ]);
+        return $response;
+    }
+
+    /**
+     * @Route("/book/create", name="create_book")
+     */
+    public function createBook(Request $request, EntityManagerInterface $em) {
+        $book = new Book();
+        $response = new JsonResponse();
+        $title = $request->get('title', null);
+        if (empty($title)) {
+            $response->setData([
+                'success' => false,
+                'error' => 'Title cannot be empty',
+                'data' => null
+            ]);
+            return $response;
+        }
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
         $response->setData([
             'success' => true,
             'data' => [
                 [
-                    'id' => 1,
-                    'title' => 'Hacia rutas salvajes'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'El nombre del viento'
-                ],
-                [
-                    'id' => 3,
-                    'title' => $title
+                    'id' => $book->getId(),
+                    'title' => $book->getTitle()
                 ]
             ]
         ]);
---
+++ src/Entity/Book.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=512, nullable=true)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
