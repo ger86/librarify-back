bd6a2019e6a172ffe9097c0a88a3c84b2bbb42a1
Gerardo Fernández <gefe.86@gmail.com>
2021-03-18 10:39:35 +0100

    feature: profiler and debug pack installed

--- librarify/config/bundles.php
+++ librarify/config/bundles.php
@@ -20,4 +20,6 @@ return [
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
