6fa8889f12fd26e5cdce37a3da6aaa9af5d40fa4
Gerardo <gerardo@latteandcode.com>
2022-05-29 14:11:34 +0200

    fix: SonataExporterSymfonyBundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
-    Sonata\Exporter\Bridge\Symfony\SonataExporterSymfonyBundle::class => ['all' => true],
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
